56e20c7f8536942a3a0358701c32fd37dc639a34
OwlWrench <62551968+OwlWrench@users.noreply.github.com>
2020-09-10 14:26:27 +0200

    Add project files.

---
+++ Weekly 2 Task 3/Weekly 2 Task 3.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+
+int main()
+{
+    
+
+    // task 3
+
+    int num = 0;
+
+    while (num < 50)
+    {
+        num+= 5;
+        std::cout << num << std::endl;
+
+    }
+    
+
+    
+    /*
+    // task 4
+
+    for (int i = 5; i <= 50; i+= 5)
+    {
+        std::cout << i << std::endl;
+    }
+    */
+
+    /*
+    // task 5
+
+    int num = 0;
+
+    do 
+    {
+        num += 5;
+        std::cout << num << std::endl;
+    } while (num < 50);
+    */
+    
+}
+
+
